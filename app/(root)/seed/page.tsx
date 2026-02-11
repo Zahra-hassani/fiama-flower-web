66c156ea521a36429c3da290c03a858030fe4535
Zahra-hassani <hassanizahra897@gmail.com>
2026-02-11 04:35:08 +0430

    data seeded(products and users)

--- app/(root)/seed/page.tsx
+++ app/(root)/seed/page.tsx
@@ -1,9 +1,23 @@
+import { SampleProducts } from '@/db/sampleProducts';
+import { prisma } from '@/lib/db'
 import React from 'react'
 
-function page() {
+async function page() {
+    await prisma.products.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+
+    await prisma.products.createMany({
+        data: SampleProducts.products
+    });
+    await prisma.user.createMany({
+        data: SampleProducts.users
+    });
   return (
     <div>
-      
+      data seeded successfully
     </div>
   )
 }
